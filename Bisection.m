71ca378bccbf16ac47a85944129cbb5da08e9161
pritikamalhotra <pritikamalhotra@rocketmail.com>
2016-04-08 12:02:51 +0530

    Bisection modified

--- Bisection.m
+++ Bisection.m
@@ -3,21 +3,23 @@ clear
 iteration=0;
 x0=input("Enter the value of x0:");
 x1=input("Enter the value of x1:");
-p0=fsolv(x0);
+Precision=input("Enter precision:");
+p0=fsolv(x0);% Function is defined in fsolv
 p1=fsolv(x1); 
 if(p0*p1>0)
-"no root between these points"
-endif
-while(abs(x1-x2)>0.00001)
-    x3 = (x1 +x2)/2;
-    x3
+"no root between these points:"
+else
+while(abs(x1-x0)> Precision)
+    x2 = (x1 +x0)/2;
+    x2
     iteration=iteration+1;
-    p3=fsolv(x3);
-    if (p1 * p3 < 0)
-        x2 = x3;
-        p2 = p3;
+    p2=fsolv(x2);
+    if (p0 * p2 < 0)
+        x1 = x2;
+        p1 = p2;
      else
-     x1 = x3;
-     p1 = p3;
+     x0 = x2;
+     p0 = p2;
     end
 end
+endif
